2fab039efaf15d7fcb1d70f24cdcd31f68c7978d
Pedro Henrique <pedropires2603@gmail.com>
2025-03-30 10:59:09 -0300

    feat: section googleplay and appstore

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -16,26 +16,54 @@ function Section({ slug, title, content, cards, sectionImages }) {
     }, [refColumn1]);
 
     function setColumn2Height() {
-        if (slug == "section_images") {
-            console.log(refColumn1?.current?.offsetHeight)
+        if (slug == "section_images")
             setHeightColumn1(refColumn1?.current?.offsetHeight - 3);
+    }
+
+    function getCustomStyle() {
+        if (slug == "section_app_links") {
+            return {
+                backgroundColor: "#3C0C60",
+                borderRadius: "24px",
+                color: "#EAE8E4",
+                padding: "40px",
+                display: "flex",
+                gap: "40px",
+            }
         }
+
+        if (slug == "section_images") {
+            return {
+                width: "100%",
+                display: "grid",
+                gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+                gap: "16px"
+            }
+        }
+
+        return {};
     }
 
     return (
         <div className={`section-container`}>
-            <div
-                style={slug == "section_images" ? {
-                    width: "100%",
-                    display: "grid",
-                    gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
-                    gap: "16px"
-                } : {}}
-            >
-                <div>
+            <div style={getCustomStyle()}>
+                <div style={{
+                    flex: 1,
+                    display: slug == "section_app_links" ? "flex" : "",
+                    alignItems: slug == "section_app_links" ? "center" : ""
+                }}>
                     <div ref={refColumn1}>
-                        <div className='title-content-container'>
-                            <h2>{title}</h2>
+                        <div
+                            style={{ marginBottom: slug == "section_app_links" ? 0 : 16 }}
+                            className='title-content-container'
+                        >
+                            <h2
+                                style={{
+                                    margin: slug == "section_app_links" ? "0 0 8px 0" : "8px 0 8px 0"
+                                }}
+                            >
+                                {title}
+                            </h2>
 
                             <div
                                 className='content-dangerously'
@@ -69,6 +97,18 @@ function Section({ slug, title, content, cards, sectionImages }) {
                         <div style={{ height: "calc(100%)" }}>
                             <img src={sectionImages[2]} />
                         </div>
+                    </div>
+                )}
+
+                {slug == "section_app_links" && (
+                    <div className='app-links-container'>
+                        <a href='https://www.apple.com/br/app-store/' target='_blank'>
+                            <img src='src\assets\images\appstore.png' />
+                        </a>
+
+                        <a href='https://play.google.com/store/games?hl=pt_BR' target='_blank'>
+                            <img src='src\assets\images\googleplay.png' />
+                        </a>
                     </div>
                 )}
             </div>
